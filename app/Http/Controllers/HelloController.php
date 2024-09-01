b403cdb14c1d8363011b0274b75a182f29c2544a
upbook1017 <upbook1017@gmail.com>
2024-09-01 23:08:11 +0900

    練習(0901)

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,25 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOF
+<style>
+body {font-size:16pt;}
+h1 { font-size:100pt;}
+</style>
+EOF;
+$body = '</head><body>';
+$end = '</body></html>';
+
+function tag($tag, $txt)
+{
+    return "<{$tag}>" . $txt . "</{$tag}>";
+}
+
 class HelloController extends Controller
 {
-    public function index($id = 'noname', $pass = 'unknown')
+    public function index()
     {
-        return <<<EOF
-<html>
-<title>hello</title>
-<style>
-body {font-size:16pt}
-</style>
-<body>
-<h1>Index</h1>
-<p>これは、Helloコントローラーのindexアクションです。</p>
-<ul>
-<li>ID: {$id}</li>
-<li>pass: {$pass}</li>
-</ul>
-</body>
-</html>
-EOF;
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title', 'Hello/Index') . $style . $body
+            . tag('h1', 'Index') . tag('p', 'this is Index page')
+            . '<a href="/hello/other">go to </a>'
+            . $end;
+        return $html;
+    }
+
+    public function other()
+    {
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title', 'Hello/Other') . $style . $body
+            . tag('h1', 'Other') . tag('p', 'aaaaaaa')
+            . $end;
+        return $html;
     }
 }
