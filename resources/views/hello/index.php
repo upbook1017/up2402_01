c9a28f6d66b4142f8702ac5d4692de41169126e0
upbook1017 <upbook1017@gmail.com>
2024-09-02 22:56:32 +0900

    練習(0902)

---
+++ resources/views/hello/index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <title>Hello</title>
+    <style>
+        body {
+            font-size: 16pt;
+        }
+
+        h1 {
+            font-size: 100pt;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Index</h1>
+    <p>これはブレードPHPです。</p>
+</body>
+
+</html>
