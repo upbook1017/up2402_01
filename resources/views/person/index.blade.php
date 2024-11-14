a7a03fc6c1cec6dbbe6fdcec4114e6e5cea6e730
upbook1017 <upbook1017@gmail.com>
2024-11-14 23:42:55 +0900

    練習1114

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -24,6 +24,17 @@
             </tr>
         @endforeach
     </table>
+
+    <table>
+        <tr>
+            <th>Data</th>
+        </tr>
+        @foreach ($items as $item)
+            <tr>
+                <td>{{ $item->getData() }}</td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
 
 @section('footer')
